94ad4d9a85881fcfa27480eb4da2442d4fbb4e74
Kaden <kho7@huskers.unl.edu>
2025-06-12 22:36:22 -0500

    Added screen classes to be implemented

--- ZeroOneMatricesTool/main.py
+++ ZeroOneMatricesTool/main.py
@@ -1,13 +1,45 @@
 from kivy.app import App
-from kivy.core.window import Window
-from kivy.modules import inspector
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
+'''
+Custom Kivy Screen Classes
+'''
+
+class HomeScreen(Screen):
+    pass
+
+class EnterMatrixScreen(Screen):
+    pass
+
+class LoadMatrixScreen(Screen):
+    pass
+
+class MatrixEditorScreen(Screen):
+    pass
+
+
+'''
+Custom Kivy Widgets Classes
+'''
+class ScreenBoxLayout(BoxLayout):
+    pass
+
+class SelfFormattingText(Label):
+    pass
+
+'''
+Define kivy app class
+'''
 
 class ZeroOneMatricesTool(App):
     def build(self):
-        inspector.create_inspector(Window, self)
-        self.sm = ScreenManager()
+        self.sm = ScreenManager(transition=NoTransition())
+        self.sm.add_widget(HomeScreen(name='HomeScreen'))
+        self.sm.add_widget(EnterMatrixScreen(name='EnterMatrixScreen'))
+        self.sm.add_widget(LoadMatrixScreen(name='LoadMatrixScreen'))
+        self.sm.add_widget(MatrixEditorScreen(name='MatrixEditorScreen'))
         return self.sm
 
 if __name__ == '__main__':
