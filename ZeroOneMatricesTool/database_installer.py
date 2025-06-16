1d1a220cf467eea9f22f14e9bf25e9b515914d77
Kaden <kho7@huskers.unl.edu>
2025-06-16 16:22:23 -0500

    Users now update information in config.py instead of directly in the database_installer.

--- ZeroOneMatricesTool/database_installer.py
+++ ZeroOneMatricesTool/database_installer.py
@@ -1,11 +1,12 @@
 from sys import stderr
 from sqlalchemy.exc import SQLAlchemyError
 from ZeroOneMatricesTool.database import MatrixDatabase
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 
 def main():
     try:
-        url = MatrixDatabase.construct_mysql_url('localhost', 3306, 'matrices', 'root', 'sqlpassword')
+        url = MatrixDatabase.construct_mysql_url(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD)
         matrix_database = MatrixDatabase(url)
         matrix_database.ensure_tables_exist()
         print('Tables created.')
